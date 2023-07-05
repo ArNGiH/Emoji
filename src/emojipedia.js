459d5ed7deb1f5d8a1d85361dfb8ccdb0c813de2
Aryan Nayar <97055976+ArNGiH@users.noreply.github.com>
2023-07-05 15:47:58 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "😠",
+    name: "Angry Face",
+    meaning:
+      "Conveys varying degrees of anger, from grumpiness and irritation to disgust and outrage. May also represent someone acting tough or being mean."
+  },
+  {
+    id: 5,
+    emoji: "🦁",
+    name: "Lion",
+    meaning:
+      "A friendly, cartoon-styled face of a lion—the large cat and king of the jungle—looking straight ahead. Depicted as a golden-yellow lion face with a light- or dark-brown mane, white muzzle, and neutral expression."
+  },
+  {
+    id: 6,
+    emoji: "💀",
+    name: "Skull",
+    meaning:
+      "A whitish-gray, cartoon-styled human skull with large, black eye sockets. Commonly expresses figurative death, e.g., dying from extreme laughter, frustration, or affection"
+  }
+];
+export default emojipedia;
